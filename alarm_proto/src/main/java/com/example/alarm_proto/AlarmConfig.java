092babf9d8d5fdd6dba8d0d1955ee78e1c2f9d8b
YooHyeonJun <71581953+YooHyeonJun@users.noreply.github.com>
2020-11-30 01:53:01 +0900

    Add files via upload

--- alarm_proto/src/main/java/com/example/alarm_proto/AlarmConfig.java
+++ alarm_proto/src/main/java/com/example/alarm_proto/AlarmConfig.java
@@ -1,50 +1,99 @@
 package com.example.alarm_proto;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import android.support.v7.app.AppCompatActivity;
+import org.w3c.dom.Text;
 
 import java.util.Calendar;
 
 public class AlarmConfig extends AppCompatActivity {
+    EditText hour;
+    EditText minute;
+    EditText estitime_input;
+    TextView recom_input;
+    Button set;
+
+
+
     AlarmManager alarm_manager; //알람 메니저
     PendingIntent pendingIntent;
-    EditText estimate_et;
 
     public void onClickAlarmConfig(View v){
+
         //알람 메니저 설정
         alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        EditText et = (EditText)findViewById(R.id.recom_input);
-        Intent routeIntent = new Intent(this, RouteConfig.class);
+        TextView et = (TextView)findViewById(R.id.recom_input);
+        Intent routeIntent = new Intent(this,RouteConfig.class);
         //알람 리시버 인텐트 생성
         final Intent al_intent = new Intent(this, Alarm_Receiver.class);
         //캘린더 객체 생성
         final Calendar calendar = Calendar.getInstance();
-        switch(v.getId()){
-            case R.id.route_sel: startActivityForResult(routeIntent,0); //경로 선택 버튼
+        switch(v.getId()) {
+            case R.id.set_time:
+                hour = (EditText) findViewById(R.id.promise_o);
+                minute = (EditText) findViewById(R.id.promise_clock);
+                estitime_input = (EditText) findViewById(R.id.estitime_input);
+                recom_input = (TextView) findViewById(R.id.recom_input);
+                set = (Button) findViewById(R.id.set_time);
+
+                String s1 = hour.getText().toString();//시 값 가져오기(xml->java)
+                String s2 = minute.getText().toString();//분 값 가져오기(xml->java)
+                String s3 = estitime_input.getText().toString();//예측 값 가져오기(xml->java)
+                int result = (Integer.parseInt(s1) * 60 + Integer.parseInt(s2) - Integer.parseInt(s3));
+                int result1 = result / 60;
+                int result2 = result % 60;
+                recom_input.setText(result1 + "시" + result2 + "분");
+
+
+                break;
+            case R.id.route_sel: startActivity(routeIntent); //경로 선택 버튼
             break;
             case R.id.alarmcon_back: this.onBackPressed(); //뒤로가기
             break;
             case R.id.config_finish: //알람 설정 완료 버튼
-            //AlarmManagement로 결과값 전송
+            //리시버에 string값 넘겨주기
+
                 String time = et.getText().toString();
-                Intent intent = new Intent(AlarmConfig.this, AlarmManagement.class);
-                if(et.getText().toString().length() == 0){
+
+                if ( et.getText().toString().length() == 0 ) {
+
                     Toast myToast = Toast.makeText(this.getApplicationContext(),"input time first", Toast.LENGTH_SHORT);
                     myToast.show();
-                }
-                else{
-                    intent.putExtra("alarm_clock",time);
-                    setResult(0,intent);
-                    finish();
+                    break;
+
+                } else {
+                    String[] arr= time.split(":");
+                    int hour, minute;
+                    hour = Integer.parseInt(arr[0]);
+                    minute = Integer.parseInt(arr[1]);
+                    calendar.setTimeInMillis(System.currentTimeMillis());
+                    calendar.set(Calendar.HOUR_OF_DAY, hour);
+                    calendar.set(Calendar.MINUTE,minute);
+                    calendar.set(Calendar.SECOND, 0);
+
+                    Toast.makeText(this,"Alarm 예정 " + hour + "시 " + minute + "분",Toast.LENGTH_SHORT).show();
+                    //이미 지나간 시간 지정이면 다음날로 지정
+                    if (calendar.before(Calendar.getInstance())) {
+                        calendar.add(Calendar.DATE, 1);
+                    }
                 }
 
+                // reveiver에 string 값 넘겨주기
+                al_intent.putExtra("state","alarm on");
+
+                pendingIntent = PendingIntent.getBroadcast(this,0,al_intent,PendingIntent.FLAG_UPDATE_CURRENT);
+                //알람 세팅
+                alarm_manager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
                 break;
         }
     }
@@ -53,30 +102,8 @@ public class AlarmConfig extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_config);
-        estimate_et = (EditText) findViewById(R.id.estitime_input);
 
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode,resultCode, data);
-        if(resultCode == 0) {
-            int val = data.getIntExtra("time",0);
-            estimate_et.setText(Integer.toString(val));
 
-        }
-    }
-    protected  void onSaveInstatnceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        //저장할 변수 여기에
-    }
-
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        if(savedInstanceState != null) {
-            //start_et.setText(savedInstanceState.getString("BACKUP_START"));
-            //finish_et.setText(savedInstanceState.getString("BACKUP_FINISH"));
-
-            //복구 작업 여기에
-        }
 
     }
 }
